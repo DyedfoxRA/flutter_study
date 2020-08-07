ff8750c54cc8a3b7d7803e530042fa24c2423073
Olia <iamola88093@gmail.com>
2020-08-07 19:37:26 +0300

    dasdas

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
+      theme: ThemeData(dsjfnsdlfjnsdf
 
         primarySwatch: Colors.blue,
       ),
